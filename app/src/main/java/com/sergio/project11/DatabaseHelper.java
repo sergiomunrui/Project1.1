244d72063a498ba1702233aea2b7b37a84c98dec
sergiomrui <sergiomunrui@gmail.com>
2023-03-26 18:47:26 +0200

    Añadida clase auxiliar DatabaseHelper

---
+++ app/src/main/java/com/sergio/project11/DatabaseHelper.java
@@ -0,0 +1,26 @@
+package com.sergio.project11;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "colegio.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
