a7789fa835ad964bff0334d23ece59cdadd2b2d5
sergiomunrui <sergio92mr@gmail.com>
2023-03-27 20:13:45 +0200

    DatabaseHelper completada con los métodos onCreate y onUpgrade

--- app/src/main/java/com/sergio/project11/DatabaseHelper.java
+++ app/src/main/java/com/sergio/project11/DatabaseHelper.java
@@ -15,12 +15,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        //Creación de las tabla de datos al iniciar el programa
+        db.execSQL("create table alumno (id_al integer primary key, nombre_al text not null, apellidos_al text not null, telefono_al text, correo_al text, grupo text not null)");
+        db.execSQL("create table asignatura (id_asig integer primary key, nombre_asig text)");
+        db.execSQL("create table profesor (id_prof integer primary key, dni text not null, nombre_prof text not null, apellidos_prof text not null, correo_prof text not null, telefono_prof text not null)");
+        db.execSQL("create table nota (id integer primary key, alumno_id integer not null, asignatura_id integer not null, trimestre integer not null, nota real not null, foreign key (alumno_id) references alumno(id_al), foreign key (asignatura_id) references asignatura(id_asig))");
+        db.execSQL("create table profesor_asignatura (profesor_id integer, asignatura_id integer, foreign key (profesor_id) references profesor(id_prof), foreign key (asignatura_id) references asignatura(id_asig), primary key (profesor_id, asignatura_id))");
+        db.execSQL("create table alumno_asignatura (alumno_id integer, asignatura_id integer, foreign key (alumno_id) references alumno(id_al),foreign key (asignatura_id) references asignatura(id_asig),primary key (alumno_id, asignatura_id))");
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS profesor");
+        db.execSQL("DROP TABLE IF EXISTS asignatura");
+        db.execSQL("DROP TABLE IF EXISTS profesor_asignatura");
+        db.execSQL("DROP TABLE IF EXISTS alumno");
+        db.execSQL("DROP TABLE IF EXISTS alumno_asignatura");
+        db.execSQL("DROP TABLE IF EXISTS nota");
+
+        onCreate(db);
 
     }
 }
